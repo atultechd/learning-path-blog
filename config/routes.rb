3e70ae6500c5bd512bc86021541b8d5296ccedbe
Atul Dwivedi <atultechd@gmail.com>
2023-08-07 20:52:58 +0530

    added published_at for draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
